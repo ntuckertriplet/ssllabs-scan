06b42e312bcc1907a9718fd0307dff7824c07c69
Nate Tucker <ntuckertriplet@gmail.com>
2019-06-06 08:29:33 -0500

    "what if no one fails?"

--- grader.py
+++ grader.py
@@ -2,6 +2,7 @@ f = open("host_grades.txt", "r")
 
 lookup_grade = ':\"F\"'
 lookup_err = ':\"Unable to connect to the server\"'
+failed = False
 
 for line in f.readlines():
     if lookup_err in line:
@@ -10,5 +11,9 @@ for line in f.readlines():
     if lookup_grade in line:
         ip_addr = line.split(":")
         print("Server received grade F at: " + str(ip_addr[0]))
+        failed = True
+
+if failed is False:
+    print("No hosts failed")
 
 f.close()
